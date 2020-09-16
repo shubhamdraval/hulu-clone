6fc0405ee662172c6036a0e974ecdb1ac4b888bc
shubhamdraval <shubhamdraval@users.noreply.github.com>
2020-09-16 16:32:57 +0530

    Added navbar :star:

--- hulu-clone/src/App.js
+++ hulu-clone/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Nav from "./Nav";
+import Results from "./Results";
 
 function App(){
     return(
         <div className="app">
             <Header />
-            {/* Nav */}
-            {/* Results */}
+            <Nav />
+            <Results />
         </div>
     ); 
 }
